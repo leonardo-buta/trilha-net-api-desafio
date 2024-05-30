ee704d1b8095e6fe31b8d282981c0b80e5b9872a
Leila Gama <leilagama@gmail.com>
2024-05-30 10:43:28 -0300

    Implementação de apis.

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -7,7 +7,6 @@ namespace TrilhaApiDesafio.Controllers
     [ApiController]
     [Route("[controller]")]
 
-    //Criar a migrations
     public class TarefaController : ControllerBase
     {
         private readonly OrganizadorContext _context;
@@ -23,14 +22,19 @@ namespace TrilhaApiDesafio.Controllers
             // TODO: Buscar o Id no banco utilizando o EF
             // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            var tarefa = _context.Tarefas.Where(x => x.Id == id);
+
+            if (tarefa == null)
+                return NotFound();
+
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var filmes = _context.Tarefas.FirstOrDefault();
+            return Ok(filmes);
         }
 
         [HttpGet("ObterPorTitulo")]
@@ -64,6 +68,9 @@ namespace TrilhaApiDesafio.Controllers
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
             // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            _context.Add(tarefa);
+            _context.SaveChanges();
+
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
